43c5a93bb9c0f45b045444f608d6713ee8e7d45c
LarsUlrich54 <pavlovi160@gmail.com>
2022-11-05 01:42:45 +0300

    Game with static balls, multiple balls available and leaderboard in a txt file, all functions are documented

--- game2.py
+++ game2.py
@@ -2,9 +2,8 @@ import pygame
 from pygame.draw import *
 from random import randint
 pygame.init()
+FPS = 2
 
-FPS = 1
-screen = pygame.display.set_mode((1200, 900))
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -13,40 +12,6 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, BLACK]
-def mouse_hit(event, balls):
-    '''checks if a mouse click has hit any balls, removes all the hit balls, and adds points for every ball hit'''
-    for ball in balls:
-        if (ball.center[0]-event.pos[0])**2 + (ball.center[1]-event.pos[1])**2 <= ball.w**2:
-            print("NICE!")
-            circle(screen, BLACK, ball.center, ball.w)
-            pts_update(ball.w)
-            balls.remove(ball)
-    return balls
-
-def pts_update(r):
-    '''adds the points to the player's score. The amount of points depends on the radius of the ball hit'''
-    global pts 
-    pts = pts + 100/r
-def pts_show(pts, name):
-    ''' shows points that the player has got'''
-    print(name, " - ", pts)
-
-def new_ball(balls):
-    '''рисует новый шарик'''
-   # global x, y, r
-    x = randint(100, 1100)
-    y = randint(100, 900)
-    r = randint(10, 100)
-    print(x,y,r)
-    color = COLORS[randint(0, 5)]
-    ball = circle(screen, color, (x, y), r)
-    balls.append(ball)
-    
-'''def mouse_action(event, balls):
-     function that is called when MOUSEBUTTONDOWN happens. 
-    print("CLICK")
-    if mouse_hit (event, balls) == True: 
-        pts_update(r)'''
 
 def player_set():
     '''sets the player name an creates his points'''
@@ -54,11 +19,61 @@ def player_set():
     name = input()
     pts = 0
 
+print ("Please, state your name")
+player_set()
+FPS = 2
+screen = pygame.display.set_mode((1200, 900))
+
+def mouse_hit(event, balls):
+    '''checks if a mouse click has hit any balls, removes all the hit balls, and adds points for every ball hit'''
+    for ball in balls:
+        if (ball[1][0]-event.pos[0])**2 + (ball[1][1]-event.pos[1])**2 <= ball[2]**2:
+            pts_update(ball[2])
+            balls.remove(ball)
+    return balls
+
+def new_ball(balls):
+    '''creates a new random ball and appends it to the list of balls'''
+    x = randint(100, 1100)
+    y = randint(100, 900)
+    r = randint(10, 100)
+    print(x,y,r)
+    color = COLORS[randint(0, 5)]
+    balls.append([color, (x,y), r])
+
+def draw_balls(balls):
+    '''draws all the balls that are on the screen at the moment'''
+    screen.fill(BLACK)
+    for ball in balls:
+        circle(screen, ball[0], ball[1], ball[2])        
+
+def pts_update(r):
+    '''updates the player's score. The amount of points depends on the radius of the ball hit'''
+    global pts 
+    pts = pts + 100/r
+
+def pts_show(pts, name):
+    '''shows points that the player has got'''
+    print(name, " - ", pts)
+
+def leaderboard_update(player, points):
+    ''' updates the leaderboard '''
+    board1 = list()
+    f = open("leaderboard.txt", "r",  encoding = "utf-8")
+    for line in f:
+        s = line.split()
+        board1.append((s[0], float(s[1])))
+    f1 = open("leaderboard.txt", "w")
+    board1.append((player, points))
+    board1.sort(key = lambda x: -x[1])
+    for player in board1:
+        f1.write("%s\n" % (player[0] + ' ' + str(player[1])))
+#main
+
 balls = list()
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
-player_set()
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
@@ -68,6 +83,7 @@ while not finished:
             print("Click")
             balls = mouse_hit(event, balls)
     new_ball(balls)
+    draw_balls(balls)
     pygame.display.update()
 pygame.quit()
-pts_show(pts, name)
+leaderboard_update(name, pts)
